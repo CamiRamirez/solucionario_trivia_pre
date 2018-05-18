197a5df3cfa7a8e50115740aa339f96649eb9073
camila <camiramirezpp@gmail.com>
2018-05-18 12:09:11 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+//evento boton
+window.onload = function() {
+    $(function(){
+        $('button.start').click(start);
+        //$('.play_again button').click(restart);
+    }
+
+    //function restart()
+    document.getElementById("name").innerText = name;
+//preguntas de la trivia
+    ("¿quieres jugar? s/n");
+   {
+        ("¿han egresado hombres en Laboratoria? s/n");
+        
+
+        ("¿hay laboratoria en concepción? s/n")
+        
+    }
+}
